ee2ccce9a7ea18ce8f3aa17a41addf2b635ee825
ubaidrmn <rehmanubaid2003@gmail.com>
2023-10-16 18:42:12 +0500

    add django channels

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ DOCKER_API_IMAGE_NAME = djangochat/api
 DOCKER_FRONTEND_IMAGE_NAME = djangochat/frontend
 
 build: docker-build-api
-run-dev-server: docker compose up
+run-dev-server: 
+	docker compose up
 
 docker-build-api:
 	docker build \
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const chatSocket = new WebSocket(
+    'ws://localhost:8000/ws/signaling/'
+  );
+
+  chatSocket.onmessage = function(e) {
+      const data = JSON.parse(e.data);
+      console.log(data)
+  };
+
+  chatSocket.onclose = function(e) {
+      console.error('Chat socket closed unexpectedly');
+  };
+
+  
+
+  async function makeCall() {
+    const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
+    const peerConnection = new RTCPeerConnection(configuration);
+    // signalingChannel.addEventListener('message', async message => {
+    //     if (message.answer) {
+    //         const remoteDesc = new RTCSessionDescription(message.answer);
+    //         await peerConnection.setRemoteDescription(remoteDesc);
+    //     }
+    // });
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    // signalingChannel.send({'offer': offer});
+    console.log(offer);
+    chatSocket.send(JSON.stringify({offer: offer}))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={makeCall}>Connect</button>
     </div>
   );
 }
