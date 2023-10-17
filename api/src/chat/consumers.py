04d5e529e3a9be7129e9908c4b7860cbd0a3c0b6
ubaidrmn <rehmanubaid2003@gmail.com>
2023-10-18 02:56:21 +0500

    update consumer class for testing

--- api/src/chat/consumers.py
+++ api/src/chat/consumers.py
@@ -14,14 +14,20 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         message = json.loads(text_data)
-        reciever = User.objects.get(reciever=message.get("reciever_id"))
-        reciever_connection = ActiveConnection.objects.get(user=reciever)
-        reciever_channel_name = reciever_connection.channel_name
-        if not ActiveConnection.objects.filter(channel_name=self.channel_name).exists:
-            ActiveConnection.objects.create(
-                channel_name=self.channel_name, 
-                user=User.objects.get(id=message.get("sender_id"))
-            )
+        sender = User.objects.get(id=message.get("sender_id"))
 
-        self.channel_layer.send(reciever_channel_name, message.get("message"))
+        if message.get("setup"):
+            if not ActiveConnection.objects.filter(channel_name=self.channel_name).exists():
+                ActiveConnection.objects.create(
+                    channel_name=self.channel_name, 
+                    user=sender)
+                self.send(text_data=json.dumps({"message": "connection with the backend established"}))
+        else:
+            reciever = User.objects.get(id=message.get("reciever_id"))
+            reciever_connection = ActiveConnection.objects.filter(user=reciever)
+            print(reciever_connection, "HERE", reciever)
+
+        # reciever_channel_name = reciever_connection.channel_name
+        # print(reciever_channel_name)
+        # self.channel_layer.send(reciever_channel_name, message.get("message"))
         # self.send(text_data=json.dumps({"message": message}))
