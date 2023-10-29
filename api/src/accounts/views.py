808ce4ca1aa3a7ee4fc4b7d7635810c2f9ab79d5
ubaidrmn <rehmanubaid2003@gmail.com>
2023-10-29 17:11:06 +0500

    feat: create user viewset

--- api/src/accounts/views.py
+++ api/src/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from accounts.models import User
+from accounts.serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for viewing and editing accounts.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # permission_classes = [IsAccountAdminOrReadOnly]
