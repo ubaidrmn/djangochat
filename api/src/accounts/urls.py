77955cd9364707fc9989d11550d91ac8a84ef060
ubaidrmn <rehmanubaid2003@gmail.com>
2023-10-29 17:12:10 +0500

    feat: add jwt authentication, create permissions file

---
+++ api/src/accounts/urls.py
@@ -0,0 +1,14 @@
+from accounts.views import UserViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.urls import path
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+urlpatterns = router.urls + [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
