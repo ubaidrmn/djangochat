a74ae69adfe89e54a2dc04c53e60d88e6510dd98
ubaidrmn <rehmanubaid2003@gmail.com>
2023-10-31 18:37:23 +0500

    feat: add user api's reducer & middleware to store

--- frontend/src/modules/app/Root/store.js
+++ frontend/src/modules/app/Root/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from "../../authentication/userSlice";
+import { userApi } from '../../authentication/userApi';
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    [userApi.reducerPath]: userApi.reducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(userApi.middleware),
 })
