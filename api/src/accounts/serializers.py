0ff480e453bb2e302d7730556797ee28b25a4225
ubaidrmn <rehmanubaid2003@gmail.com>
2023-10-31 17:53:50 +0500

    feat: add password field to user serializer

--- api/src/accounts/serializers.py
+++ api/src/accounts/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from accounts.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+    )
+
+    def create(self, validated_data):
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'fullname', 'bio']
+        fields = ['id', 'username', 'email', 'fullname', 'bio', 'password']
--- api/src/accounts/views.py
+++ api/src/accounts/views.py
@@ -2,11 +2,32 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from accounts.models import User
 from accounts.serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from accounts.permissions import UserPermissions
+
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    A simple ViewSet for viewing and editing accounts.
-    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [IsAccountAdminOrReadOnly]
+    # permission_classes = [UserPermissions]
+
+    # @action(
+    #     detail=False,
+    #     methods=['post'],
+    #     url_path="register-user",
+    #     url_name="register-user"
+    # )
+    # def register_user(self, request):
+    #     print("HAHA")
+    #     serializer = self.serializer_class(data=request.data)
+    #     if serializer.is_valid():
+    #         # serializer.save()
+    #         print("HAHA")
+    #         # user = User.objects.get(serializer.validated_data['id'])
+    #         # user.set_password(serializer.validated_data['password'])
+    #         # user.save()
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     else:
+    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
